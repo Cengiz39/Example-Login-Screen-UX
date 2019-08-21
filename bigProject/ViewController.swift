b3d021e57226559d3ee542d091b1c9f3db08b630
Cengiz Baygın <qrowlite02@icloud.com>
2019-08-21 22:38:34 +0300

    Initial Commit

---
+++ bigProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  bigProject
+//
+//  Created by Cengiz Baygın on 21.08.2019.
+//  Copyright © 2019 Cengiz Baygın. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
